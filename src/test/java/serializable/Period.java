dd90444d04254e2160a0509f71e1ea3df4ab43cf
hjliu <hjliu@thoughtworks.com>
2016-02-20 15:07:50 +0800

    Exception occurs when the order in readObject and writeObject is inconsistent

--- src/test/java/serializable/Period.java
+++ src/test/java/serializable/Period.java
@@ -6,31 +6,52 @@ import java.io.ObjectInputStream;
 import java.io.Serializable;
 import java.util.Date;
 
-public class Period implements Serializable{
+public class Period implements Serializable {
     public static long serialVersionUID = 4647424730390249716l;
-    private final Date start;
-    private final Date end;
+    private Date start;
+    private Date end;
+
     /**
      * @param start the beginning of the period
-     * @param end the end of the period; must not precede start
+     * @param end   the end of the period; must not precede start
      * @throws IllegalArgumentException if start is after end
-     * @throws NullPointerException if start or end is null
+     * @throws NullPointerException     if start or end is null
      */
     public Period(Date start, Date end) {
-        this.start = new Date(start.getTime()-100000);
+        this.start = new Date(start.getTime() - 100000);
         this.end = new Date(end.getTime());
         if (this.start.compareTo(this.end) > 0)
             throw new IllegalArgumentException(
                     start + " after " + end);
     }
-    public Date start () { return new Date(start.getTime()); }
-    public Date end () { return new Date(end.getTime()); }
-    public String toString() { return start + " - " + end; }
+
+    public Date start() {
+        return new Date(start.getTime());
+    }
+
+    public Date end() {
+        return new Date(end.getTime());
+    }
+
+    public String toString() {
+        return start + " - " + end;
+    }
+
     private void readObject(ObjectInputStream s)
             throws IOException, ClassNotFoundException {
         s.defaultReadObject();
+        this.end = (Date) s.readObject();
+        this.start = (Date) s.readObject();
         // Check that our invariants are satisfied
         if (start.compareTo(end) > 0)
-            throw new InvalidObjectException(start +" after "+ end);
+            throw new InvalidObjectException(start + " after " + end);
+    }
+
+    private synchronized void writeObject(java.io.ObjectOutputStream s) throws IOException {
+        s.defaultWriteObject();
+        s.writeObject(this.start);
+        s.writeObject(this.end);
+        if (start.compareTo(end) > 0)
+            throw new InvalidObjectException(start + " after " + end);
     }
 }
