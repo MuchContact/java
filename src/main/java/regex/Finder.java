aace67b2384c89136afa5da6a5d6d49f2477c991
hjliu <hjliu@thoughtworks.com>
2016-04-09 20:03:17 +0800

    提取查找函数,新增测试用例

--- src/main/java/regex/Finder.java
+++ src/main/java/regex/Finder.java
@@ -4,12 +4,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Finder {
-    public static void main(String[] args) {
-        Pattern p = Pattern.compile("(\\d\\d)(\\d)");
-        Matcher m = p.matcher("a123b");
-        while(m.find()){
-            System.out.println(m.group());
+    public String findFirst(String pattern, String source) {
+        String result = "";
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(source);
+        if (m.find()) {
+            result = m.group(1);
         }
-        System.out.println(m.matches());
+        return result;
     }
 }
