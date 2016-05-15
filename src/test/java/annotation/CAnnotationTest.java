4b706d086157a7a252d8e86b2bf854720a8368cb
MuchContact <liuhengjiangis@foxmail.com>
2016-05-15 16:29:21 +0800

    通过标准输出测试注解类运行状况

--- src/test/java/annotation/CAnnotationTest.java
+++ src/test/java/annotation/CAnnotationTest.java
@@ -1,17 +1,40 @@
 package annotation;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 public class CAnnotationTest {
+
+    private ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+    @Before
+    public void before() throws Exception {
+        System.setOut(new PrintStream(output));
+    }
+
+    @After
+    public void after() throws Exception {
+        System.setOut(null);
+    }
+
     @Test
     public void should_invoke_before_method() throws Exception {
 //        IMonitor monitorOne = mock(MonitorOne.class);
         ProxyTarget target = ProxyFactory.proxy(ProxyTarget.class);
         target.doSomething();
+        String result = output.toString();
+        assertThat(result.contains("MonitorOne report: start to do something"), is(true));
+        assertThat(result.contains("MonitorOne report: done for doing something"), is(true));
 //        verify(monitorOne, times(1)).before();
     }
 
