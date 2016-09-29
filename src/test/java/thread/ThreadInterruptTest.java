b4bfe8a0e25ce1ddd2ad75ac8e0b4f62e0e67b68
MuchContact <liuhengjiangis@foxmail.com>
2016-09-29 16:45:31 +0800

    remove useless imports

--- src/test/java/thread/ThreadInterruptTest.java
+++ src/test/java/thread/ThreadInterruptTest.java
@@ -1,10 +1,7 @@
 package thread;
 
-import org.junit.*;
 import org.junit.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertTrue;
 
 public class ThreadInterruptTest {
